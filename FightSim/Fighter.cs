fe6a60d202e14240b48ea0899d94f4e5ad8be96b
gammeflamme <wilhelm.flamman@gmail.com>
2019-10-17 16:26:19 +0200

    e nivå

---
+++ FightSim/Fighter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightSim
+{
+    class Fighter
+    {
+
+        int hp = 20;
+        public string name;
+        static Random rand = new Random();
+
+
+        public int Attack()
+        {
+            return rand.Next(2,4);
+        }
+        public void Hurt(int amount)
+        {
+
+            hp -= amount;
+
+        }
+        public bool IsAlive()
+        {
+
+            return hp > 0;
+
+        }
+        public int GetHp()
+        {
+            return hp;
+        }
+    }
+}
---
+++ FightSim/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightSim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter name of player 1 :");
+            Fighter player1 = new Fighter();
+            player1.name = Console.ReadLine();
+            Console.WriteLine("enter name of player 2 :");
+            Fighter player2 = new Fighter();
+            player2.name = Console.ReadLine();
+
+
+
+            while(player1.IsAlive() && player2.IsAlive())
+            {
+                player2.Hurt(player1.Attack());
+                player1.Hurt(player2.Attack());
+
+
+            }
+            if (player1.IsAlive())
+            {
+                Console.WriteLine("Congrats " + player1.name);
+
+            }
+            else if (player2.IsAlive())
+            {
+                Console.WriteLine("Congrats " + player2.name);
+
+            }
+            else
+            {
+                Console.WriteLine("Good job you both died");
+
+            }
+        }
+    }
+}
